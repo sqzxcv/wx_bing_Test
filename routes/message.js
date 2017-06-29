e692df1126e19288d0331f83d0fa05edaadca03d
ShengQiang <qiang_sheng@qq.com>
2017-06-29 17:06:01 +0800

    修改测试消息

--- routes/message.js
+++ routes/message.js
@@ -6,7 +6,7 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
 
     res.contentType('json');
-    res.send(JSON.stringify({status:'success','type':'string'}));
+    res.send(JSON.stringify({status:'success','msg-type':'string', content:'hello boy!'}));
     res.end();
 });
 
